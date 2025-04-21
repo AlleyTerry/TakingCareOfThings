4ea0f4ac4ddc3992682f5d78145bd81cddc02f3c
AlleyTerry <art8689@nyu.edu>
2025-04-21 13:41:46 -0400

    overhaul... again

--- TakingCareOfThings/Assets/Scripts/OfferingMinigame/OrganizingMinigame.cs
+++ TakingCareOfThings/Assets/Scripts/OfferingMinigame/OrganizingMinigame.cs
@@ -11,7 +11,11 @@ public class OrganizingMinigame : MonoBehaviour
 {
     //[SerializeField] private List<GameObject> blurbs = new List<GameObject>();
     public Offering objectNameType;
-   [SerializeField ]  private Dictionary<string, string> blurbs = new Dictionary<string, string>();
+    [SerializeField] private Dictionary<string, string> blurbs = new Dictionary<string, string>();
+   [SerializeField ]  private Dictionary<string, string> Childishblurbs = new Dictionary<string, string>();
+    [SerializeField] private Dictionary<string, string> Homebodyblurbs = new Dictionary<string, string>();
+    [SerializeField] private Dictionary<string, string> Outgoingblurbs = new Dictionary<string, string>();
+    [SerializeField] private Dictionary<string, string> Adventurousblurbs = new Dictionary<string, string>();
     public TextMeshProUGUI blurbText;
     private string randomKey;
     private string itemKey;
@@ -26,16 +30,53 @@ public class OrganizingMinigame : MonoBehaviour
     public float timer;
     public TextMeshProUGUI timerText;
     public bool timerStarted = false;
-  
+    public PersonalityType personalityType;
+    [SerializeField]public enum PersonalityType
+    {
+        //types of person
+        Childish,
+        Homebody,
+        Outgoing,
+        Adventurous
+    }
+
+    private int randomPersonality;
     // Start is called before the first frame update
     void Start()
     {
-        DoneDoneButton.SetActive(false);
         
-        blurbs.Add("childish", "He never seemed to grow up");
-        blurbs.Add("homebody", "You would never catch her at the club");
-        blurbs.Add("outgoing", "He was always the life of the party");
-        blurbs.Add("adventurous", "She was always looking for the next thrill");
+        DoneDoneButton.SetActive(false);
+        //set the enum to a random value
+        randomPersonality = Random.Range(0, 4);
+        personalityType = (PersonalityType)randomPersonality;
+        
+        //ADD the blurbs based on the personality type
+        if (personalityType == PersonalityType.Childish)
+        {
+            blurbs.Add("childish", "They really liked trains...");
+            blurbs.Add("childish2", "They were never too old for cartoons...");
+            blurbs.Add("childish3", "He thought he was the biggest joker..."); 
+            blurbs.Add("childish4", "His he always had a lolipop in his mouth...");
+            
+        }
+        else if (personalityType == PersonalityType.Homebody)
+        {
+            blurbs.Add("homebody", "You would never catch them at the club");
+            blurbs.Add("homebody2", "Their room was always so cozy");
+            blurbs.Add("homebody3", "They had the best taste in movies");
+            blurbs.Add("homebody4", "You could always count on them for a good reccomendation");
+            
+        }
+        else if (personalityType == PersonalityType.Outgoing)
+        {
+            blurbs.Add("outgoing", "He was always the life of the party");
+
+        }
+        else if (personalityType == PersonalityType.Adventurous)
+        {
+            blurbs.Add("adventurous", "She was always looking for the next thrill");
+        }
+
         int randomNum = Random.Range(0, blurbs.Keys.Count-1);
         //print(randomNum);
         randomKey = blurbs.Keys.ElementAt(randomNum);
